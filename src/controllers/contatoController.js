9e9e268cd48d5a52ddc84628f36cea52f597b76e
guimatds <guilherme.mat.ds@gmail.com>
2024-08-29 02:09:54 -0300

    Criando cadastro de cadastro

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,38 @@
-exports.paginaInicial = (req, res) => {
-    res.send(`Obrigado por entrar em contato!`);
+const Contato = require('../models/ContatoModel');
+
+exports.index = (req, res) => {
+    res.render('contato', {
+        contato: {}
+    });
+};
+
+// precisa ser async pois função retorna promise (banco de dados)
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body);
+        await contato.register();
+        
+        if(contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('index'));
+            return;
+        }
+    
+        req.flash('success', 'Contato registrado com sucesso');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.editIndex = async function (req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const contato = await Contato.buscaPorId(req.params.id);
+    if(!contato)return res.render('404');
+
+    res.render('contato', { contato });
 };
